fecdb78d639c88e0d3f61841f901250a7456d848
paula martins <lea.paulinha@gmail.com>
2025-02-12 14:03:22 -0300

    Fazendo exer da aula 2

--- Aula2/Aula2/Program.cs
+++ Aula2/Aula2/Program.cs
@@ -53,4 +53,83 @@ static void Exer2(string[] args)
     Console.WriteLine("Média: {0} ", media);
 }
 
-Exer2(args);
+//Exercicio 3
+//Faça um programa que recebe nomes de diferentes animais
+//Cada vez que o usuário digitar o nome de um animal, o programa deve
+//checar se o nome já foi digitado anteriormente. Se foi, então deve ser exibido
+//uma mensagem alertando que o nome já foi digitado
+static void Exer3(string[] args)
+{
+    List<string> animais = new List<string>();
+    string nomeTemp;
+    bool parar = false;
+
+    while(parar == false)
+    {
+        Console.WriteLine("Escreva nome de um animal");
+        nomeTemp = Console.ReadLine();
+
+        for (int i = 0; i < animais.Count; i++)
+        {
+            if (nomeTemp == animais[i])
+            {
+                Console.WriteLine("JÁ FOI INSERIDO ESSE ANIMAL");
+            }
+        }
+        animais.Add(nomeTemp);
+
+        Console.WriteLine("Escreva true Se quiser parar ou false para continuar");
+        parar = Convert.ToBoolean(Console.ReadLine());
+
+    }
+
+    Console.WriteLine("Sua lista:");
+    for (int i = 0; i < animais.Count; i++)
+    {
+     Console.WriteLine("{0}", animais[i]);
+    }
+}
+
+static void Exer4(string[] args)
+{
+    List<string> tarefas = new List<string>();
+    string tarefaTemp;
+    int opcao = 0, numTarefa;
+
+    Console.WriteLine("Bem vindo a sua lista de tarefas");
+    while (opcao != 4){
+        Console.WriteLine("1-Excluir Tarefa");
+        Console.WriteLine("2-Adicionar Tarefa");
+        Console.WriteLine("3-Listar tarefas");
+        Console.WriteLine("4-Sair");
+
+        opcao = Convert.ToInt32(Console.ReadLine());
+
+        switch (opcao)
+        {
+            case 1:
+                Console.WriteLine("Qual tarefa você quer excluir?");
+                numTarefa = Convert.ToInt32(Console.ReadLine());
+                tarefas.RemoveAt(numTarefa - 1);
+                break;
+            case 2:
+                Console.WriteLine("Qual tarefa você quer adicionar?");
+                tarefaTemp = Console.ReadLine();
+                tarefas.Add(tarefaTemp);
+                break;
+            case 3:
+                Console.WriteLine("------------------");
+                for (int i = 0; i < tarefas.Count; i++)
+                {
+                    Console.WriteLine("{0}) {1}", i + 1, tarefas[i]);
+                }
+                Console.WriteLine("------------------");
+                break;
+            case 4:
+                break;
+        }
+    }
+    
+}
+
+Exer4(args);
