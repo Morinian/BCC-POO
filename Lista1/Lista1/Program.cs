37d0a7d4498cf4f9768fdd382b24f0ae9163a3fc
PAULA MARTINS DE SOUZA <lea.paulinha@gmail.com>
2025-02-18 20:18:13 -0300

    adicionando enunciado

--- Lista1/Lista1/Program.cs
+++ Lista1/Lista1/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System;
+using System.Collections.Generic; //Biblioteca para utilização da lista
+using System.IO; //Biblioteca para utilização de leitura de arquivo
+
 
 //Exercício 1 - Calculadora
 
@@ -134,6 +138,49 @@ static void Exer2(string[] args)
 
 }
 
+//Exercício 3 - Jogar Dados
+//Objetivo: usando uma aplicação do tipo console do dotnet, criar um jogo simplificado onde
+//dois jogadores podem jogar dados e a aplicação informa qual dos jogadores tirou o maior
+//número. Faça o controle de versionamento da sua aplicação com git e suba a mesma para
+//o github.
+//Novos conceitos ou funcionalidades desse exercício
+//Usaremos nesse exercício a classe Random do dotnet para simular uma jogada de dados
+//com 6 lados. O random irá possibilitar que tiremos um número aleatório (de 1 a 6 para
+//nosso exemplo). Você pode usar loop para esse exercício, estude sobre loops caso não
+//conheça. Poderá também optar por usar recursão (o exemplo de consulta usa recursão no
+//método "IniciarRodadas"), recomendo estudar sobre recursão também antes de iniciar o
+//exercício.
+//Requisitos para desenvolver a aplicação
+//● Quando o usuário abrir o jogo deverá ser solicitado o nome do primeiro e do
+//segundo jogador. Não será possível jogar com mais do que dois jogadores.
+//● O jogo deverá solicitar um nome para cada jogador e os nome não podem ser
+//iguais.
+//● O jogo terá 3 rodadas, o jogador que tirar o maior número no dados na maioria das
+//jogadas vence a partida.
+//● Em caso de empate (onde os dois tiram o mesmo número) nenhum jogador
+//pontuará.
+//● A cada rodada a aplicação deve informar quem foi o jogador vencedor. Exemplos de
+//mensagens:
+//○ João tirou o número 2 e Maria o número 6. Maria venceu a rodada 1.
+//○ João tirou o número 1 e Maria o número 1. Empate.
+//● Um placar sempre deve ser exibido até o final do jogo para que os jogadores
+//acompanhem o resultado. Exemplo: Pontos do jogador Maria: 1 Pontos do jogador
+//João: 0
+//● Ao final da rodada 3 a aplicação deve informar quem foi o vencedor ou se foi um
+//empate.
+//Opcional
+//● Fazer o controle de versionamento da sua aplicação usando git;
+//● Subir a aplicação para um repositório do github.
+//Desafios
+//1. Se a terceira rodada terminar em empate, faça com que os jogadores continuem
+//jogando dados. O próximo a pontuar ganha o jogo.
+//2. Crie uma opção antes de iniciar o jogo que permite que o usuário
+
+static void Exer6(string[] args)
+{
+
+}
+
 //Exercício 6 - Jogo da Forca
 //Objetivo: usando uma aplicação do tipo console do dotnet, criar um jogo da forca que irá ler
 //uma lista de nomes de filmes, nomes de jogos (vídeo game) e países. Com essa aplicação
@@ -142,6 +189,7 @@ static void Exer2(string[] args)
 //Novos conceitos ou funcionalidades desse exercício
 //Gravação e leitura de arquivos para armazenar os nomes de filmes, carros e países.
 //Estude:
+
 //● Gravação e leitura de arquivos.
 //● Orientação a objetos (encapsulamento, herança, abstração e polimorfismo).
 //Caso não conheça os conceitos de orientação a objetos, pause um pouco a execução do
@@ -159,6 +207,7 @@ static void Exer2(string[] args)
 //substituídas por underline (_). Exemplo:
 //○ Se a palavra for o filme Matrix, deverá ser exibido 6 underline no lugar da
 //palavra.
+
 //○ Ao acertar a letra A, deverá ser exibido apenas a letra a na posição correta.
 //● As palavras devem ficar armazenadas em um arquivo de texto do tipo CSV que terá
 //a palavra seguido pela categoria, onde Filme = 0, Jogo = 1, e Pais = 2. Segue um
@@ -166,6 +215,7 @@ static void Exer2(string[] args)
 //○ Matrix; 0;
 //○ Batman; 1;
 //○ Brasil; 2;
+
 //● Se alguma palavra possui letra com acentuação, ela deve ser reconhecida quando o
 //usuário informar sem acentuação, além disso a aplicação não deve diferenciar
 //maiúsculo de minúsculo. Estude como usar o System.Text.Encoding.CodePages
@@ -182,6 +232,7 @@ static void Exer2(string[] args)
 //● Utilize Enumeradores para a categoria das palavras.
 //● Estude o tratamento de erros com C# (Exceptions).
 //● Estude a biblioteca System.Linq; para facilitar sua manipulação de listas.
+
 //Desafios
 //1. Crie mais uma categoria para o jogo dê sua preferência.
 //2. Antes de iniciar o jogo dê a opção de o usuário escolher a dificuldade do jogo: fácil,
