4eb5489e955307bcffbb2d1ef70275fb5c35de5b
paula martins <lea.paulinha@gmail.com>
2025-03-16 18:03:08 -0300

    ADD exer 1

---
+++ Lista2/Lista2/Funcionario.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Lista2
+{
+    public class Funcionario
+    {
+        public string nome;
+        public string cpf;
+        public double salarioBruto;
+        public double salarioLiquido;
+
+
+        public void Relatorio(Funcionario funcionario)
+        {
+            Console.WriteLine("---------RELATORIO---------");
+            Console.WriteLine("Nome: {0}", funcionario.nome);
+            Console.WriteLine("CPF: {0}", funcionario.cpf);
+
+            if(funcionario.salarioBruto < 2000)
+            {
+                funcionario.salarioLiquido = funcionario.salarioBruto;
+                Console.WriteLine("Nada foi descontado\nSalario Bruto: {0} \nSalario Liquido: {1}",funcionario.salarioBruto, funcionario.salarioLiquido);
+            }else if( funcionario.salarioBruto >= 2000.01 && funcionario.salarioBruto <= 3000)
+            {
+                funcionario.salarioLiquido = funcionario.salarioBruto - (funcionario.salarioBruto * 0.075);
+                Console.WriteLine("7,5% foi descontado\nSalario Bruto: {0} \nSalario Liquido: {1}", funcionario.salarioBruto, funcionario.salarioLiquido);
+            }
+            else if (funcionario.salarioBruto >= 3000.01 && funcionario.salarioBruto <= 4000)
+            {
+                funcionario.salarioLiquido = funcionario.salarioBruto - (funcionario.salarioBruto * 0.15);
+                Console.WriteLine("15% foi descontado\nSalario Bruto: {0} \nSalario Liquido: {1}", funcionario.salarioBruto, funcionario.salarioLiquido);
+            }
+            else if (funcionario.salarioBruto >= 4000.01 && funcionario.salarioBruto <= 5000)
+            {
+                funcionario.salarioLiquido = funcionario.salarioBruto - (funcionario.salarioBruto * 0.225);
+                Console.WriteLine("22,5% foi descontado\nSalario Bruto: {0} \nSalario Liquido: {1}", funcionario.salarioBruto, funcionario.salarioLiquido);
+            }
+            else if (funcionario.salarioBruto > 5000.01)
+            {
+                funcionario.salarioLiquido = funcionario.salarioBruto - (funcionario.salarioBruto * 0.275);
+                Console.WriteLine("27,5% foi descontado\nSalario Bruto: {0} \nSalario Liquido: {1}", funcionario.salarioBruto, funcionario.salarioLiquido);
+            }
+        }
+    }
+}
+
---
+++ Lista2/Lista2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Lista2;
+
+
+namespace Lista2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exer1();
+        }
+
+        //Uma empresa precisa calcular os salários de seus funcionários.Sabe-se que um
+        //funcionário possui um nome, cpf e salário.Um funcionário também tem seu salário líquido,    
+        //que se baseia no seu salário, descontando impostos, a partir do valor do salário do
+        //funcionário:
+        //- Até R$ 2000: sem descontos
+        //- De R$ 2000,01 a R$ 3000: 7,5%
+        //- De R$ 3000,01 a R$ 4000: 15%
+        //- De R$ 4000,01 a R$ 5000: 22,5%
+        //- Acima de R$ 5000,01: 27,5%
+        //A empresa quer poder calcular o total do pagamento no mês.No relatório, deve estar
+        //contido o pagamento para cada funcionário, incluindo o salário bruto, a quantidade
+        //descontada, e o salário líquido.No final, deve ser apresentado o total do salário bruto dos
+        //funcionários, o total descontado, e o total do salário líquido.
+
+        static void Exer1()
+        {
+            Funcionario funcionario = new Funcionario();
+
+            Console.WriteLine("Escreva nome do funcionario");
+            funcionario.nome = Console.ReadLine();
+            Console.WriteLine("Escreva o CPF");
+            funcionario.cpf = Console.ReadLine();
+            Console.WriteLine("Escreva o salario bruto");
+            funcionario.salarioBruto = Convert.ToDouble(Console.ReadLine());
+
+            funcionario.Relatorio(funcionario);
+        }
+    }
+}
