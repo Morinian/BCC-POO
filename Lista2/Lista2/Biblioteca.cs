ff1ca305726054f264c8e970f0706976b7449611
paula martins <lea.paulinha@gmail.com>
2025-03-18 23:18:45 -0300

    ADD exer2

--- Lista2/Lista2/Biblioteca.cs
+++ Lista2/Lista2/Biblioteca.cs
@@ -11,9 +11,31 @@ namespace Lista2
         string nome;
         List<Livro> livros;
 
+        public Biblioteca()
+        {
+            livros = new List<Livro>();
+        }
+
         public void Adicionar(Livro livro)
         {
             livros.Add(livro);
+        }
+
+        public void Alugar(Livro livro)
+        {
+            livros.Remove(livro);
+        }
+
+        public void listarLivros()
+        {
+            for(int i = 0; i < livros.Count; i++)
+            {
+                Console.WriteLine("Livro: {0}", livros[i].titulo);
+                Console.WriteLine("Autor: {0}", livros[i].autor);
+                Console.WriteLine("Anor: {0}", livros[i].ano);
+                Console.WriteLine("-----------  ");
+
+            }
         }
     }
 }
--- Lista2/Lista2/Program.cs
+++ Lista2/Lista2/Program.cs
@@ -34,7 +34,41 @@ namespace Lista2
         //imprimir a lista de livros, incluindo o título, autor e ano de publicação
         static void Exer2()
         {
+            Biblioteca nacional = new Biblioteca();
 
+            Livro harry = new Livro();
+            Livro escocia = new Livro();
+            Livro logica = new Livro();
+            Livro leroy = new Livro();
+
+            harry.autor = "Mulher autora";
+            escocia.autor = "Escocia autor";
+            logica.autor = "Claro";
+            leroy.autor = "Leroy Autor";
+
+            harry.ano = "2026";
+            escocia.ano = "2029";
+            logica.ano = "1988";
+            leroy.ano = "1890";
+
+            harry.titulo = "Harry e a pedra";
+            escocia.titulo = "Escocia um mundo novo";
+            logica.titulo = "Logica basica";
+            leroy.titulo = "Leroy a menina que iria longe";
+
+            nacional.Adicionar(harry);
+            nacional.Adicionar(escocia);
+            nacional.Adicionar(logica);
+            nacional.Adicionar(leroy);
+
+            nacional.listarLivros();
+
+            Console.WriteLine("\n");
+            nacional.Alugar(harry);
+            nacional.Alugar(leroy);
+            Console.WriteLine("\n");
+
+            nacional.listarLivros();
         }
     }
 }
