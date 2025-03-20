ef6aa30c94229f8b68d89a3038a9b3e32eef3cf3
paula martins <lea.paulinha@gmail.com>
2025-03-19 22:53:00 -0300

    ADD aula 5 de exer1

---
+++ Aula5/Aula5/Funcionario.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula5
+{
+    class Funcionario
+    {
+        private string nome;
+        private string sobrenome;
+        private double salario;
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public string Sobrenome
+        {
+            get { return sobrenome; }
+            set { sobrenome = value; }
+        }
+        public double Salario
+        {
+            get { return salario; }
+            set { salario = value; }
+        }
+
+        public Funcionario(string nome, string sobrenome, double salario)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Salario = salario;
+        }
+
+        public void imprimirFuncionario()
+        {
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Nome: {0}", nome);
+            Console.WriteLine("Sobrenome: {0}", sobrenome);
+            Console.WriteLine("Salario anual: {0}", salario*12);
+        }
+    }
+}
---
+++ Aula5/Aula5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aula5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Exer1();
+        }
+
+        static void Exer1()
+        {
+            Funcionario Paula = new Funcionario("Paula", "Martins", 1500);
+            Funcionario Gabriel = new Funcionario("Gabriel", "Ferreira", 8000);
+            Funcionario Fabio = new Funcionario("Fabio","Vidal",15000);
+
+            Paula.imprimirFuncionario();
+            Gabriel.imprimirFuncionario();
+            Fabio.imprimirFuncionario();
+        }
+    }
+}
